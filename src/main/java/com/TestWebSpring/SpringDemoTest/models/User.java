9f6d385cb205c8ecce00a2cae9aae0d8677c4993
Eugene <1995@mail.ru>
2023-11-26 11:23:40 +0300

    Added Administation pannnel. New features. Remove bugs

--- src/main/java/com/TestWebSpring/SpringDemoTest/models/User.java
+++ src/main/java/com/TestWebSpring/SpringDemoTest/models/User.java
@@ -6,9 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "users")
@@ -38,6 +36,10 @@ public class User implements  UserDetails{
     joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    private List<Product> products = new ArrayList<>();
+
     private LocalDateTime dateOfCreated;
 
     @PrePersist
@@ -48,6 +50,9 @@ public class User implements  UserDetails{
 
     //secutiry
 
+    public boolean isAdmin() {
+        return roles.contains(Role.ROLE_ADMIN);
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
--- src/main/java/com/TestWebSpring/SpringDemoTest/services/ProductService.java
+++ src/main/java/com/TestWebSpring/SpringDemoTest/services/ProductService.java
@@ -2,13 +2,16 @@ package com.TestWebSpring.SpringDemoTest.services;
 
 import com.TestWebSpring.SpringDemoTest.models.Image;
 import com.TestWebSpring.SpringDemoTest.models.Product;
+import com.TestWebSpring.SpringDemoTest.models.User;
 import com.TestWebSpring.SpringDemoTest.repositories.ProductRepository;
+import com.TestWebSpring.SpringDemoTest.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.List;
 
 @Service
@@ -16,13 +19,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     public List<Product> listProducts(String title) {
         if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+    public void saveProduct(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        product.setUser(getUserByPrincipal(principal));
         Image image1;
         Image image2;
         Image image3;
@@ -40,10 +45,15 @@ public class ProductService {
             image3.setPreviewImage(true);
             product.addImageToProduct(image3);
         }
-        log.info("Saving new Product. Title: {}; Author:{}", product.getTitle(), product.getAuthor());
+        log.info("Saving new Product. Title: {}; Author email:{}", product.getTitle(), product.getUser().getEmail());
         Product productFromDB = productRepository.save(product);
         productFromDB.setPreviewImageId(productFromDB.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        if(principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
     }
 
 
